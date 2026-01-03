e70bb82699e610f9630eca170c230f2cb56375b1
Tree-0 <117487029+Tree-0@users.noreply.github.com>
2026-01-02 18:46:22 -0600

    [FEAT] basic GET and POST gcal events

--- adapters/gcal_io.py
+++ adapters/gcal_io.py
@@ -21,7 +21,10 @@ from googleapiclient.errors import HttpError
 
 class GoogleCalendarIO:
     def __init__(self):
-        self.SCOPES = ["https://www.googleapis.com/auth/calendar.events.owned"]
+        self.SCOPES = [
+            "https://www.googleapis.com/auth/calendar.events.owned", 
+            "https://www.googleapis.com/auth/calendar"
+        ]
         
         self.__credentials = self.__get_credentials()
         if not self.__credentials:
@@ -29,11 +32,41 @@ class GoogleCalendarIO:
 
     # Not sure how many of these methods will be necessary, and some will probably be redesigned
 
-    def send_event_to_calendar(calendar_id: str, event: GoogleCalendarEvent) -> bool:
-        pass
+    # TODO: return bool or just event_result? what is event_result type, dict?
+    def send_event_to_calendar(self, calendar_id: str, event: GoogleCalendarEvent):
+        try:
+            service = build("calendar", "v3", credentials=self.__credentials)
+            
+            event = {
+                'summary': event.summary,
+                'description': event.description,
+                'start': {
+                    'dateTime': event.start_dt.isoformat(),
+                    'timeZone': 'America/Chicago',
+                },
+                'end': {
+                    'dateTime': event.end_dt.isoformat(),
+                    'timeZone': 'America/Chicago',
+                },
+            }
+            
+            event_result = service.events().insert(calendarId=calendar_id, body=event).execute()
 
-    def send_events_to_calendar(calendar_id: str, events: List[GoogleCalendarEvent]) -> bool:
-        pass
+            return event_result
+        
+        except Exception as e:
+            print(e)
+            return False
+
+    # TODO: look into batching? This would not save on API quota, and network overhead is probably not going to ever be a bottleneck,
+    # so might not be a big deal.
+    def send_events_to_calendar(self, calendar_id: str, events: List[GoogleCalendarEvent]) -> bool:
+        for event in events:
+            try:
+                self.send_event_to_calendar(calendar_id, event)
+            except Exception as e:
+                print(e)
+                continue
 
     def sync_google_calendar(calendar: GoogleCalendar) -> bool:
         # get id from obj, write all events to the calendar if they are not already there?
@@ -44,10 +77,58 @@ class GoogleCalendarIO:
     def read_google_calendar(calendar_id: str, ) -> bool:
         pass
 
-    def __get_all_calendars() -> List[GoogleCalendar]:
+    # TODO: consider... do I really need custom GoogleCalendar and GoogleCalendarEvent objects,
+    # or do the objects returned by the resource builder work well enough such that my custom
+    # objects are redundant? 
+    def get_all_calendars(self):
+        # get all of the authenticated user's calendars
+        try:
+            service = build("calendar", "v3", credentials=self.__credentials)
+
+            print("Getting calendars...")
+            result = (service.calendarList().list()).execute()
+
+            for cal in result.get('items', []):
+                print(cal['summary'])
+                print('     ' + cal['id'])
+                print('     ' + cal['accessRole'])
+            
+            return result
+
+        except Exception as e:
+            print(e)
+    
+    def get_calendar_events(self, after_dt: datetime, calendar_id: str, limit=100):
+        # get the events from a particular calendar after a date
+        try:
+            service = build("calendar", "v3", credentials=self.__credentials)
+
+            # str format for request body
+            dt = after_dt.isoformat()
+            
+            print(f"Getting events from {calendar_id} after {after_dt.isoformat()}")
+            events_result = (
+                service.events()
+                .list(
+                    calendarId=calendar_id,
+                    timeMin=dt,
+                    maxResults=limit,
+                    singleEvents=True,
+                    orderBy="startTime",
+                )
+                .execute()
+            )
+            events = events_result.get("items", [])
+
+            if not events:
+                print("No upcoming events found.")
+                return []
+            
+            return events
         
-        
-        pass
+        except Exception as e:
+            print(e)
+            return []
 
     # Modified starter code taken from quickstart documentation of google calendar API
     def __get_credentials(self) -> Credentials:
@@ -79,3 +160,30 @@ class GoogleCalendarIO:
 
 if __name__ == "__main__":
     gcal_io = GoogleCalendarIO()
+    result = gcal_io.get_all_calendars()
+    [print(item["summary"]) for item in result.get("items", [])]
+    
+    first_cal = result["items"][3]["id"]
+    
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
+    events = gcal_io.get_calendar_events(now, first_cal, 10)
+
+    for event in events:
+        print(event["summary"])
+        #print(f"    {event['start']['dateTime']} - {event['end']['dateTime']}")
+        print(f"    {event['start']} - {event['end']}")
+        print(type(event['start']))
+
+    test_event = GoogleCalendarEvent(
+        id='', summary="TEST EVENT API INSERT",
+        description="TEST DESCRIPTION",
+        start_dt = datetime.datetime.now(tz=datetime.timezone.utc),
+        end_dt = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(hours=2)
+    )
+
+    print("sending test event to calendar ", first_cal)
+    insert_event_result = gcal_io.send_event_to_calendar(first_cal, test_event)
+
+    print(insert_event_result)
+
+
--- data_models/gcal.py
+++ data_models/gcal.py
@@ -2,10 +2,20 @@
 
 from dataclasses import dataclass
 from typing import List
+import datetime
 
 @dataclass
 class GoogleCalendarEvent:
     # will fill this out to be compatible with API 
+    id: str
+    summary: str
+    description: str
+    
+    # assume UTC time
+    start_dt: datetime
+    end_dt: datetime
+
+    colorId: str = ''
 
     # need some constructor that takes an event.Event object and translates the data
     pass
@@ -13,4 +23,6 @@ class GoogleCalendarEvent:
 @dataclass
 class GoogleCalendar:
     # ditto
+    id: str
+
     events: List[GoogleCalendarEvent]
